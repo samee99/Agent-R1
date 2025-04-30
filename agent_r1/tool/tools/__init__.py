cf903563e9d9bcf9d53b60e70698ce71150b73b7
yrr <624451680@qq.com>
2025-04-30 09:05:21 +0000

    update

--- agent_r1/tool/tools/__init__.py
+++ agent_r1/tool/tools/__init__.py
@@ -1,11 +1,13 @@
-# from agent_r1.tool.tools.search_tool import SearchTool
+from agent_r1.tool.tools.search_tool import SearchTool
 from agent_r1.tool.tools.python_tool import PythonTool
 from agent_r1.tool.tools.wiki_search_tool import WikiSearchTool
 
 def _default_tool(name):
     if name == "search":
+        return SearchTool()
+    elif name == "wiki_search":
         return WikiSearchTool()
     elif name == "python":
         return PythonTool()
     else:
-        raise NotImplementedError
+        raise NotImplementedError(f"Tool {name} not implemented")
